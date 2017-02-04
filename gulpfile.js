ac06b08f46e5854ec16c4a13aec393dd4b5fad0b
Joel <joelbcarter@gmail.com>
2017-02-03 22:11:33 -0600

    Initial setup for tests using Gulp, Mocha, & Chai

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp'),
+    mocha = require('gulp-mocha');
+
+gulp.task('test', () =>
+    gulp.src('./test/*.test.js', { read: false })
+        .pipe(mocha({ reporter: 'spec' }))
+);
